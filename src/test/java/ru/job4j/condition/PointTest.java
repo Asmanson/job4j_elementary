688b7d53af65319a925f638a2e68a0843a0061d7
Иван Асманов <Asmanovvv@yandex.ru>
2022-10-04 01:28:48 +0300

    2.1. Рефакторинг - Расстояние между точками. [#122587]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -1,51 +1,25 @@
 package ru.job4j.condition;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class PointTest {
 
     @Test
-    public void when00to20then2() {
-        double expected  = 2;
-        int x1 = 0;
-        int x2 = 0;
-        int y1 = 2;
-        int y2 = 0;
-        double out = Point.distance(x1, x2, y1, y2);
-        Assert.assertEquals(expected, out, 0.01);
+    public void when00to02then2() {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double dist = a.distance(b);
+        double expected = 2.0;
+        assertThat(dist).isEqualTo(expected);
     }
 
     @Test
-    public void when03to40then5() {
-        double expected  = 5;
-        int x1 = 0;
-        int x2 = 3;
-        int y1 = 4;
-        int y2 = 0;
-        double out = Point.distance(x1, x2, y1, y2);
-        Assert.assertEquals(expected, out, 0.01);
-    }
-
-    @Test
-    public void when01to11then1() {
-        double expected  = 1;
-        int x1 = 0;
-        int x2 = 1;
-        int y1 = 1;
-        int y2 = 1;
-        double out = Point.distance(x1, x2, y1, y2);
-        Assert.assertEquals(expected, out, 0.01);
-    }
-
-    @Test
-    public void when50to010then11dot18() {
-        double expected  = 11.18;
-        int x1 = 5;
-        int x2 = 0;
-        int y1 = 0;
-        int y2 = 10;
-        double out = Point.distance(x1, x2, y1, y2);
-        Assert.assertEquals(expected, out, 0.01);
+    public void when40to03then5() {
+        Point a = new Point(4, 0);
+        Point b = new Point(0, 3);
+        double dist = a.distance(b);
+        double expected = 5.0;
+        assertThat(dist).isEqualTo(expected);
     }
 }
